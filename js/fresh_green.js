5c991f5184043bfcab32af248ff5c9b25e583ba9
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2012-08-21 17:23:20 -0300

    Table style adjustments

--- js/fresh_green.js
+++ js/fresh_green.js
@@ -1,8 +1,7 @@
 (function ($) {
   Drupal.behaviors.fresh_green = {
     attach: function(context) {
-      $("tr:odd", context).addClass("odd");
-      console.log('chamou?')
+      $("table.zebra tr:not(:has(th)):even", context).addClass("even");
     }
   }
 })(jQuery);
